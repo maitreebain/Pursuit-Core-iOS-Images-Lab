81c235d9973e770c3381406cfcd8dd25482db691
maitreebain <maitreebain@gmail.com>
2019-12-10 17:36:36 -0500

    created Ui working on getting images and data

---
+++ Images-Lab/Images-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Images-Lab
+//
+//  Created by Maitree Bain on 12/10/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
