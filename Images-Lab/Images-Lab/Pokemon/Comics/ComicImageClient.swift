0224a7569c2e187b3ab623a994c54c11dba545fe
maitreebain <maitreebain@gmail.com>
2019-12-11 14:56:49 -0500

    pokemon data structs made, work on getting pokemon data

--- Images-Lab/Images-Lab/Pokemon/Comics/ComicImageClient.swift
+++ Images-Lab/Images-Lab/Pokemon/Comics/ComicImageClient.swift
@@ -12,6 +12,30 @@ struct ComicImageClient {
     
     static func getComicImage(for urlString: String, completion: @escaping (Result<UIImage, AppError>) -> ()) {
         
-    }
+        guard let url = URL(string: urlString) else {
+            print("no image url found \(urlString)")
+            return
+        }
+        
+            let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+               
+                if let error = error {
+                    print("error: \(error)")
+                }
+                
+                if let data = data {
+                    guard let image = UIImage(data: data) else {
+                        print("no image found")
+                        return
+                    }
+                    
+                    completion(.success(image))
+                    
+                }
+               
+                
+            }
+            dataTask.resume()
+        }
     
 }
---
+++ Images-Lab/Images-Lab/Pokemon/PokemonDataLoad.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonDataLoad.swift
+//  Images-Lab
+//
+//  Created by Maitree Bain on 12/11/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonDataLoad: Decodable {
+    let cards: [Cards]
+}
+
+struct Cards: Decodable {
+    let name: String
+    let imageUrlHiRes: String
+    let types: [String]
+    let weakness: Weakness
+    let set: String
+}
+
+struct Weakness: Decodable {
+    let type: String
+    let value: String
+}
