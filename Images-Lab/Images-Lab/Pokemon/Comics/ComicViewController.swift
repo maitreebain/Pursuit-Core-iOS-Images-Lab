5972313346e7c6ec481b6a4bac48ee7374713c9b
maitreebain <maitreebain@gmail.com>
2019-12-11 14:14:40 -0500

    adding all project files

---
+++ Images-Lab/Images-Lab/Pokemon/Comics/ComicViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ComicViewController.swift
+//  Images-Lab
+//
+//  Created by Maitree Bain on 12/11/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+
+}
---
+++ Images-Lab/Images-Lab/Pokemon/PokemonAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonAPIClient.swift
+//  Images-Lab
+//
+//  Created by Maitree Bain on 12/11/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
